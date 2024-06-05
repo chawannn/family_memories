6929583c53a34b885bf0acc2b48190d8c3c3f18b
chawannn <158204549+chawannn@users.noreply.github.com>
2024-06-05 08:50:41 +0000

    2024/06/05

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,98 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Admin.create!(email: "", password: "")
+
+families = [
+  [
+    {    
+      email: "yamada@test.com",
+      password: "password",
+      name: "yamada",
+      name_hiragana: "test",
+      birthday: "2000/10/10",
+      nickname: "test",
+      blood_type: :b,
+      letter_color: "#01e3f4"
+    },
+    { 
+      email: "yamada_a@test.com",
+      password: "password",
+      name: "yamada_a",
+      name_hiragana: "test1",
+      birthday: "2020/12/12",
+      nickname: "test1",
+      blood_type: :a,
+      letter_color: "#01e3f4",
+    },
+    { 
+      email: "yamada_b@test.com",
+      password: "password",
+      name: "yamada_b",
+      name_hiragana: "test1",
+      birthday: "2020/12/12",
+      nickname: "test1",
+      blood_type: :a,
+      letter_color: "#01e3f4",
+    }
+  ],
+  [
+    {    
+      email: "tanaka@test.com",
+      password: "password",
+      name: "tanaka",
+      name_hiragana: "test",
+      birthday: "2000/10/10",
+      nickname: "test",
+      blood_type: :b,
+      letter_color: "#01e3f4"
+    },
+    { 
+      email: "tanaka_a@test.com",
+      password: "password",
+      name: "tanaka_a",
+      name_hiragana: "test1",
+      birthday: "2020/12/12",
+      nickname: "test1",
+      blood_type: :a,
+      letter_color: "#01e3f4",
+    },
+    {    
+      email: "tanaka_b@test.com",
+      password: "password",
+      name: "tanaka_b",
+      name_hiragana: "test",
+      birthday: "2000/10/10",
+      nickname: "test",
+      blood_type: :b,
+      letter_color: "#01e3f4"
+    },
+    { 
+      email: "tanaka_c@test.com",
+      password: "password",
+      name: "tanaka_c",
+      name_hiragana: "test1",
+      birthday: "2020/12/12",
+      nickname: "test1",
+      blood_type: :a,
+      letter_color: "#01e3f4",
+    }
+  ],
+]
+
+families.each do |members|
+  members.each.with_index do |member, i|
+    members = []
+    if i == 0
+      @owner = Member.create!(member)
+    else
+      member = Member.new(member)
+      member.invited_by_id = @owner.id
+      member.save
+      mambers << member
+    end
+  end
+end
+
+
